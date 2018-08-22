d95b5919f067a0754bf732c0b2c5f91abf0b1be1
mmaayylliiss <maylis.agniel@gmail.com>
2018-08-22 16:36:01 +0200

    🚚 Rename new-website > md-starter-theme (theme folder)

--- app/wp-content/themes/new-website/404.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$context = Timber::get_context();
-Timber::render('404.twig', $context);
--- app/wp-content/themes/new-website/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Template used to display default page (Legal notices, personal data)
-
-$context         = Timber::get_context();
-$post            = new TimberPost();
-$context['post'] = $post;
-
-Timber::render(['page.twig'], $context);
