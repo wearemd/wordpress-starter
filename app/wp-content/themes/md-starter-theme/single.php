e0174a5e7b77f095658831f29c6618e1c0a6eb92
Awea <aweaoftheworld@gmail.com>
2019-12-11 10:38:05 +0100

    :wrench: Use single.twig for post

---
+++ app/wp-content/themes/md-starter-theme/single.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Page
+ *
+ * Template used to display default page (Legal notices, personal data)
+ *
+ * @package MD_Starter_Theme
+ */
+
+$context         = Timber::get_context();
+$context['post'] = new TimberPost();
+
+Timber::render( [ 'single.twig' ], $context );
